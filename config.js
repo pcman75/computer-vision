4a18c64d8748b8f9269bdbb404de847cfe88ab2f
Cosmin Manoliu <manolic@cosmins-mbp.lan>
2020-05-21 00:22:54 +0300

    write openalpr raw data to mongodb

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const config = {
     mongodb_url: `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0-vute7.gcp.mongodb.net`,
     dbName: 'openalpr',
+    rawDataCol: 'raw',
     
     prowlApiUrl: 'https://api.prowlapp.com/publicapi/add'
 };
--- index.js
+++ index.js
@@ -3,18 +3,30 @@ require('dotenv').config();
 const config = require('./config.js');
 const MongoClient = require('mongodb').MongoClient;
 
-exports.openalprhook = (request, response) => {
-  switch (request.get('content-type')) {
-    // '{"name":"John"}'
-    case 'application/json':
-      console.log("Got json");
-      console.log(JSON.stringify(request.body, null, 4));
-      break;
+exports.openalprhook = async (request, response) => {
 
-    default:
-      console.error("I don't know what request is this");
+  let client;
+  try {
+    switch (request.get('content-type')) {
+      case 'application/json':
+        client = await MongoClient.connect(config.mongodb_url);
+        let db = client.db(config.dbName);
+        await db.collection(config.rawDataCol).insert(request.body);
+        break;
+
+      default:
+        console.error("I don't know what request is this");
+    }
+    response.status(200).send(request.body);
   }
-  response.status(200).send('Hello World!');
+  catch (err) {
+    response.status(400).send(err);
+    console.error(err);
+  }
+  finally {
+    if (client)
+      await client.close();
+  }
 };
 
 exports.event = (event, callback) => {
